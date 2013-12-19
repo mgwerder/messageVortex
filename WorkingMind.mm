fc38ba764dd93aec887919dbe94b1e06d800193b
mgwerder <mgwerder>
2013-12-19 07:11:24 +0000

    Updated AnonBiB

---
+++ WorkingMind.mm
@@ -0,0 +1,67 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1201229246234" ID="Freemind_Link_721208657" MODIFIED="1387381544481" TEXT="MailVortex">
+<node CREATED="1387381549512" ID="_" MODIFIED="1387381555994" POSITION="right" TEXT="Terms">
+<node CREATED="1387381556813" ID="Freemind_Link_1627035857" MODIFIED="1387381561329" TEXT="SURB">
+<node CREATED="1387381561283" ID="Freemind_Link_719503040" MODIFIED="1387381570237" TEXT="Single use Reply Block"/>
+</node>
+<node CREATED="1387381581899" ID="Freemind_Link_763415908" MODIFIED="1387381595081" TEXT="unobservability"/>
+<node CREATED="1387381597351" ID="Freemind_Link_1223853174" MODIFIED="1387381602109" TEXT="anonymity"/>
+<node CREATED="1387381622506" ID="Freemind_Link_1652100981" MODIFIED="1387381634753" TEXT="Censorship-resistant"/>
+<node CREATED="1387381742419" ID="Freemind_Link_1048305390" MODIFIED="1387381833634" TEXT="parrot circumvention syststems (oakland2013-parrot)&#xa;&#xa;"/>
+</node>
+<node CREATED="1387381552110" ID="Freemind_Link_1000957008" MODIFIED="1387383141058" POSITION="right" TEXT="Protocol design goals"/>
+<node CREATED="1387382157426" ID="Freemind_Link_119419384" MODIFIED="1387382162675" POSITION="left" TEXT="Requirements">
+<node CREATED="1387382162629" ID="Freemind_Link_557085012" MODIFIED="1387382175764" TEXT="unobservable peers">
+<node CREATED="1387382176443" ID="Freemind_Link_412729679" MODIFIED="1387382180936" TEXT="encryption"/>
+</node>
+<node CREATED="1387382183471" ID="Freemind_Link_764087696" MODIFIED="1387382192191" TEXT="Censorship resitant">
+<node CREATED="1387382192729" ID="Freemind_Link_1193092763" MODIFIED="1387382227659" TEXT="Parrot flaging function (please answer to embedding mail)"/>
+</node>
+</node>
+<node CREATED="1387382381376" ID="Freemind_Link_891870390" MODIFIED="1387382387850" POSITION="left" TEXT="classification">
+<node CREATED="1387382388295" ID="Freemind_Link_449362361" MODIFIED="1387382396329" TEXT="adversary">
+<node CREATED="1387382396883" ID="Freemind_Link_328496998" MODIFIED="1387382401048" TEXT="active">
+<node CREATED="1387382414769" ID="Freemind_Link_1443046881" MODIFIED="1387382429916" TEXT="censorship"/>
+</node>
+<node CREATED="1387382401961" ID="Freemind_Link_1514579709" MODIFIED="1387382405003" TEXT="passive">
+<node CREATED="1387382446546" ID="Freemind_Link_708279407" MODIFIED="1387382451523" TEXT="espionage"/>
+<node CREATED="1387382466624" ID="Freemind_Link_1028832596" MODIFIED="1387382482458" TEXT="user classification"/>
+</node>
+</node>
+<node CREATED="1387382534774" ID="Freemind_Link_891081098" MODIFIED="1387382538768" TEXT="attack">
+<node CREATED="1387382885599" ID="Freemind_Link_1851088936" MODIFIED="1387382887425" TEXT="passive">
+<node CREATED="1387382776179" ID="Freemind_Link_1416753797" MODIFIED="1387382793121" TEXT="Information disclosure thru normative behaveour"/>
+<node CREATED="1387382673225" ID="Freemind_Link_488424366" MODIFIED="1387382741164" TEXT="Information disclosure thru weak encryption"/>
+<node CREATED="1387382563492" ID="Freemind_Link_601341093" MODIFIED="1387382613179" TEXT="information disclosure thru failures (eg retries)"/>
+<node CREATED="1387382549085" ID="Freemind_Link_1016494609" MODIFIED="1387382602477" TEXT="information disclosure thru traffic analysis"/>
+</node>
+<node CREATED="1387382888836" ID="Freemind_Link_440617946" MODIFIED="1387382891567" TEXT="active">
+<node CREATED="1387382614529" ID="Freemind_Link_1407969948" MODIFIED="1387382630722" TEXT="Information disclosure thru elimination">
+<node CREATED="1387382934366" ID="Freemind_Link_589342771" MODIFIED="1387382937580" TEXT="replay"/>
+<node CREATED="1387382938991" ID="Freemind_Link_1020856387" MODIFIED="1387382953703" TEXT="dropping"/>
+<node CREATED="1387382954241" ID="Freemind_Link_1896854915" MODIFIED="1387382958359" TEXT="injecting"/>
+</node>
+<node CREATED="1387383049258" ID="Freemind_Link_846441053" MODIFIED="1387383051333" TEXT="DoS">
+<node CREATED="1387383057207" ID="Freemind_Link_1123249969" MODIFIED="1387383061263" TEXT="flooding"/>
+</node>
+<node CREATED="1387382540273" ID="Freemind_Link_773646780" MODIFIED="1387382595052" TEXT="information diclosure thru tagging"/>
+</node>
+<node CREATED="1387382892760" ID="Freemind_Link_445518583" MODIFIED="1387382896785" TEXT="proactive">
+<node CREATED="1387382988444" ID="Freemind_Link_629228820" MODIFIED="1387382992251" TEXT="probing"/>
+</node>
+</node>
+<node CREATED="1387382761795" ID="Freemind_Link_468619327" MODIFIED="1387382764681" TEXT="MIX">
+<node CREATED="1387382843143" ID="Freemind_Link_472515780" MODIFIED="1387382858525" TEXT="chaum Mix"/>
+</node>
+<node CREATED="1387382803628" ID="Freemind_Link_1721382903" MODIFIED="1387382807824" TEXT="identity">
+<node CREATED="1387382808534" ID="Freemind_Link_1937576675" MODIFIED="1387382817925" TEXT="real world identity"/>
+<node CREATED="1387382819914" ID="Freemind_Link_167700104" MODIFIED="1387382822956" TEXT="email"/>
+<node CREATED="1387382825289" ID="Freemind_Link_363267168" MODIFIED="1387382834696" TEXT="aliasID"/>
+</node>
+</node>
+<node CREATED="1387383146167" ID="Freemind_Link_8421634" MODIFIED="1387383158398" POSITION="right" TEXT="evaluation">
+<node CREATED="1387383158858" ID="Freemind_Link_542217584" MODIFIED="1387383171416" TEXT="SMTP cannot be shut down"/>
+</node>
+</node>
+</map>
