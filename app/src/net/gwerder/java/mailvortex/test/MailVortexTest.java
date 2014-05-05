ee28979a105e1ae1dad59bbdcc07e7c3bda22caa
mgwerder <mgwerder>
2014-05-05 07:35:49 +0000

    Added missing junit files and test suite

---
+++ app/src/net/gwerder/java/mailvortex/test/MailVortexTest.java
@@ -0,0 +1,27 @@
+package net.gwerder.java.mailvortex.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/**
+ * Tests for {@link MailVortex}.
+ *
+ * @author martin@gwerder.net (Martin GWERDER)
+ */
+@RunWith(JUnit4.class)
+public class MailVortexTest {
+
+    @Test
+    public void thisAlwaysPasses() {
+
+    }
+
+    @Test
+    @Ignore
+    public void thisIsIgnored() {
+    }
+}
---
+++ app/src/net/gwerder/java/mailvortex/test/MainSuite.java
@@ -0,0 +1,14 @@
+package net.gwerder.java.mailvortex.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+  MailVortexTest.class
+})
+
+public class MainSuite {
+  // the class remains empty,
+  // used only as a holder for the above annotations
+}
