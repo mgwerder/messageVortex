3f4dd09c30bf369ccd2f7f477e82d6e90248e66e
mgwerder <mgwerder>
2014-05-13 11:50:42 +0000

    Added rudimentary Connection manager

--- app/src/imap/ImapSSLSocket.java
+++ app/src/imap/ImapSSLSocket.java
@@ -3,52 +3,148 @@ package net.gwerder.java.mailvortex.imap;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Arrays;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.concurrent.ConcurrentSkipListSet;
 import java.util.Scanner;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
- 
 import javax.net.ssl.SSLServerSocket;
 import javax.net.ssl.SSLServerSocketFactory;
 import javax.net.ssl.SSLSession;
 import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLException;
 import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.X509KeyManager;
+import javax.net.ssl.SSLContext;
  
 /**
  * @author Martin Gwerder
  */
 public class ImapSSLSocket {
- 
-    public static final int PORT=5678;
-    
-    /**
+
+	/**
      * Selftest function for IMAP server.
      *
      * @param args      ignored commandline arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Server s=new Server(143);
         Executors.newSingleThreadExecutor().execute(new Client());
     }
+	
+	static class ImapConnection extends StoppableThread implements Comparable<ImapConnection> {
+	
+		static private final int CONNECTION_NOT_AUTHENTICATED = 1;
+		static private final int CONNECTION_AUTHENTICATED     = 2;
+		static private final int CONNECTION_SELECTED          = 3;
+		
+		SSLSocket sock;
+		private int status=CONNECTION_NOT_AUTHENTICATED;
+		
+		public ImapConnection(SSLSocket sock) 
+		{
+			this.sock=sock;	
+            SSLSession sslSession = sock.getSession();
+            String cipherSuite = sslSession.getCipherSuite();
+            System.out.println("Server got connection secured with "+cipherSuite);
+			Executors.newSingleThreadExecutor().execute(this);
+		}
+		
+		public int compareTo(ImapConnection i) {
+			if(this==i) {
+				return 0;
+			}else {
+				return -1;
+			}	
+		}
+		
+        public int shutdown() {
+            // FIXME implementation missing
+            return 0;
+        }
+        
+        public void run() {
+			try{
+				Scanner scanner = new Scanner(sock.getInputStream());
+				System.out.println("Reading...");
+				String got="";
+				try{
+					while (true) {
+						try{
+							System.out.println("waiting for full line");
+							while(!scanner.hasNextLine()) { 
+								Thread.sleep(10); 
+							}
+						} catch(InterruptedException e) {}	
+						got=scanner.nextLine();
+						System.out.println("Server received: " + got);
+					}
+				} catch(RuntimeException e) {}	
+				System.out.println("Server stopped reading...");
+				scanner.close();
+				sock.close();
+			} catch(IOException e) {
+				e.printStackTrace();
+			}	
+		}
+		
+	}
  
     static class ImapListener extends StoppableThread  {
         
         private int port=143;
         SSLServerSocket serverSocket;
+		Set<ImapConnection> conn;
         
-        public ImapListener(int port) {
+        public ImapListener(int port,Set<ImapConnection> conn) 
+			throws java.security.NoSuchAlgorithmException,java.security.KeyManagementException,java.security.GeneralSecurityException,IOException
+		{
+			this.conn=conn;
             this.port=port;
-            try{
-                this.serverSocket = (SSLServerSocket) SSLServerSocketFactory.getDefault().createServerSocket(port);
-                System.out.println("Server ready..." + serverSocket);
-                System.out.println("Supported Cipher Suites: " + Arrays.toString(((SSLServerSocketFactory) SSLServerSocketFactory.getDefault()).getSupportedCipherSuites()));
-            } catch(IOException e) {
-                //FIXME unable to allocate port
-                e.printStackTrace();
-            }
+			SSLContext sctx=SSLContext.getInstance("TLS"  );
+			sctx.init(new X509KeyManager[] {new CustomKeyManager("../../keystore.jks","changeme", "mykey2") }, null, null);
+            System.out.println("Server ready..." + serverSocket);	
+            System.out.println("Supported Cipher Suites: ");
+			String[] arr=((SSLServerSocketFactory) sctx.getServerSocketFactory().getDefault()).getSupportedCipherSuites(); 
+			HashSet<String> suppCiphers=new HashSet<String>();
+			for(int i=0;i<arr.length;i++) {
+				System.out.print("   - "+arr[i]+" ");	
+				boolean supported=true;
+				try{ 
+					this.serverSocket = (SSLServerSocket) sctx.getServerSocketFactory().getDefault().createServerSocket(port);
+					this.serverSocket.setEnabledCipherSuites(new String[] {arr[i]});
+					// Fix blocking socket
+					Executors.newSingleThreadExecutor().execute(new Thread() {
+						{
+							try{
+								Thread.sleep(100);
+								SSLSocket cs = (SSLSocket) SSLSocketFactory.getDefault().createSocket("localhost", 143);
+							} catch(Exception e) {}
+						}
+					});
+					SSLSocket s=(SSLSocket)serverSocket.accept();
+					s.close();
+				} catch(SSLException e) {supported=false;}
+				if(supported) {
+					suppCiphers.add(arr[i]);
+					System.out.println("(appropriate key found)");
+				} else {
+					System.out.println("(NO KEY)");
+				}
+				this.serverSocket.close();
+			}
+            this.serverSocket = (SSLServerSocket) sctx.getServerSocketFactory().getDefault().createServerSocket(port);
+			for(int i=0;i<suppCiphers.toArray().length;i++) {
+				System.out.println("Supported cypher: "+(suppCiphers.toArray(new String[0]))[i]);
+			}
+			this.serverSocket.setEnabledCipherSuites(suppCiphers.toArray(new String[0]));
         }
         
         public int shutdown() {
-            // FIXME implementation missing
+            try{
+				this.serverSocket.close();
+			} catch(Exception e) {};
             return 0;
         }
         
@@ -59,16 +155,9 @@ public class ImapSSLSocket {
                 while(!shutdown) {
                     socket = (SSLSocket) serverSocket.accept();
                     // FIXME fork scanner thread off
-                    SSLSession sslSession = socket.getSession();
-                    String cipherSuite = sslSession.getCipherSuite();
-                    System.out.println(cipherSuite);
-                    scanner = new Scanner(socket.getInputStream());
-                    System.out.println("Reading...");
-                    while (scanner.hasNextLine()) {
-                        System.out.println("Server received: " + scanner.nextLine());
-                    }
-                    scanner.close();
-                    socket.close();
+					ImapConnection imc=null;
+					imc=new ImapConnection(socket);
+					conn.add(imc);
                     socket=null;
                 }
                 serverSocket.close();
@@ -92,16 +181,21 @@ public class ImapSSLSocket {
 
     static class Server {
         
+		static Set<ImapConnection> conn=new ConcurrentSkipListSet<ImapConnection>();
         private int port;
         private ImapListener listener;
         
-        public Server() {
+        public Server() 
+			throws java.security.NoSuchAlgorithmException,java.security.KeyManagementException,java.security.GeneralSecurityException,IOException
+		{
             this(143);
         }
         
-        public Server(int port) {
+        public Server(int port) 
+			throws java.security.NoSuchAlgorithmException,java.security.KeyManagementException,java.security.GeneralSecurityException,IOException
+		{
             this.port = port;
-            listener  = new ImapSSLSocket.ImapListener(port);
+            listener  = new ImapSSLSocket.ImapListener(port,conn);
             Executors.newSingleThreadExecutor().execute(listener);
         }
         
@@ -112,14 +206,16 @@ public class ImapSSLSocket {
         public void run() {
             try {
  
-                SSLSocket socket = (SSLSocket) SSLSocketFactory.getDefault().createSocket("localhost", PORT);
+                SSLSocket socket = (SSLSocket) SSLSocketFactory.getDefault().createSocket("localhost", 143);
                 PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
                 System.out.println("Client -> sending...");
                 for (int i = 0; i < 5; i++) {
-                    String message = "Hallo: " + i;
-                    System.out.println("Client sent: " + message);
-                    printWriter.println(message);
+                    String message = "Hallo " + i;
+                    System.out.println("Client sending: " + message);
+                    printWriter.println(message+"\r\n");
                     printWriter.flush();
+                    System.out.println("Client sent: " + message);
+                    System.out.println("sleeping...");
                     TimeUnit.SECONDS.sleep(1);
                 }
                 socket.close();
--- doc/mailvortex.tps
+++ doc/mailvortex.tps
@@ -17,8 +17,8 @@ Class=LaTeXView
 Document=mailvortex.tex
 
 [Frame0_View0,0]
-TopLine=39
-Cursor=436
+TopLine=0
+Cursor=8647
 
 [Frame1]
 Flags=0
@@ -32,11 +32,11 @@ NormalPos.top=26
 NormalPos.right=1562
 NormalPos.bottom=540
 Class=LaTeXView
-Document=inc\parts\theory.tex
+Document=inc\parts\intro.tex
 
 [Frame1_View0,0]
-TopLine=177
-Cursor=10852
+TopLine=0
+Cursor=0
 
 [Frame2]
 Flags=0
@@ -50,11 +50,11 @@ NormalPos.top=26
 NormalPos.right=1562
 NormalPos.bottom=540
 Class=LaTeXView
-Document=inc\parts\current.tex
+Document=inc\parts\theory.tex
 
 [Frame2_View0,0]
-TopLine=0
-Cursor=0
+TopLine=214
+Cursor=10852
 
 [Frame3]
 Flags=0
@@ -68,7 +68,7 @@ NormalPos.top=26
 NormalPos.right=1562
 NormalPos.bottom=540
 Class=LaTeXView
-Document=inc\parts\intro.tex
+Document=inc\parts\current.tex
 
 [Frame3_View0,0]
 TopLine=0
@@ -86,11 +86,11 @@ NormalPos.top=26
 NormalPos.right=1562
 NormalPos.bottom=540
 Class=LaTeXView
-Document=inc\parts\design.tex
+Document=inc\parts\analysis.tex
 
 [Frame4_View0,0]
-TopLine=132
-Cursor=7002
+TopLine=17
+Cursor=1474
 
 [Frame5]
 Flags=0
@@ -104,13 +104,31 @@ NormalPos.top=26
 NormalPos.right=1562
 NormalPos.bottom=540
 Class=LaTeXView
-Document=inc\parts\analysis.tex
+Document=inc\parts\design.tex
 
 [Frame5_View0,0]
-TopLine=17
-Cursor=1474
+TopLine=132
+Cursor=7002
+
+[Frame6]
+Flags=0
+ShowCmd=1
+MinPos.x=-1
+MinPos.y=-1
+MaxPos.x=-1
+MaxPos.y=-1
+NormalPos.left=4
+NormalPos.top=26
+NormalPos.right=1562
+NormalPos.bottom=540
+Class=LaTeXView
+Document=inc\parts\verification.tex
+
+[Frame6_View0,0]
+TopLine=0
+Cursor=0
 
 [SessionInfo]
-FrameCount=6
-ActiveFrame=0
+FrameCount=7
+ActiveFrame=6
 
