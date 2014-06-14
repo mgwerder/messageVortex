f138789a8214ee460ecdccb9f72757b2e436c9d4
mgwerder <mgwerder>
2014-06-14 20:08:32 +0000

    Updated legacy test suite

--- app/src/test/MainSuite.java
+++ app/src/test/MainSuite.java
@@ -1,7 +1,9 @@
 package net.gwerder.java.mailvortex.test;
 
 import org.junit.runner.RunWith;
+import junit.framework.TestSuite;
 import org.junit.runners.Suite;
+import junit.framework.JUnit4TestAdapter;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
@@ -13,7 +15,11 @@ import org.junit.runners.Suite;
 public class MainSuite {
 
   public static junit.framework.Test suite() {    
-	return new junit.framework.JUnit4TestAdapter(MailVortexTest.class);
+        final TestSuite s = new TestSuite();
+		s.addTest(new JUnit4TestAdapter(MailVortexTest.class));
+        s.addTest(new JUnit4TestAdapter(ImapCommandTest.class));
+        s.addTest(new JUnit4TestAdapter(ImapCommandLoginTest.class));
+        return s;
   }
   // the class remains empty,
   // used only as a holder for the above annotations
