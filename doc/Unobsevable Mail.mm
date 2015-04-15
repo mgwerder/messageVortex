17b9babf240fb78067f44f2246006825d8211530
mgwerder <mgwerder>
2015-04-15 04:05:28 +0000

    added poster

---
+++ doc/Unobsevable Mail.mm
@@ -0,0 +1,94 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1428680220878" ID="ID_413561441" MODIFIED="1428682421590" TEXT="Unobsevable Mail">
+<node CREATED="1428680234381" ID="ID_86168253" MODIFIED="1428681988862" POSITION="right" TEXT="Why hard?">
+<node CREATED="1428680240140" ID="ID_669148658" MODIFIED="1428682155159" TEXT="Unidentifiable">
+<icon BUILTIN="full-4"/>
+<node CREATED="1428680251459" ID="ID_393034307" MODIFIED="1428680262440" TEXT="If identifiable it can be blocked"/>
+<node CREATED="1428680399988" ID="ID_806132664" MODIFIED="1428680414122" TEXT="Must cloak in other traffic">
+<node CREATED="1428680414828" ID="ID_1371916684" MODIFIED="1428680422992" TEXT="Avoid the dead parrot"/>
+</node>
+<node CREATED="1428680900892" ID="ID_1291076326" MODIFIED="1428680925833" TEXT="Must not be a direct connection between peers"/>
+<node CREATED="1428681818433" ID="ID_396773943" MODIFIED="1428681828133" TEXT="Must maintain k-anonymity"/>
+<node CREATED="1428682433466" ID="ID_557756000" MODIFIED="1428682457383" TEXT="Must not be able to be traced thru time based analysis"/>
+</node>
+<node CREATED="1428680292994" ID="ID_231802746" MODIFIED="1428681995943" TEXT="Untagable">
+<icon BUILTIN="full-3"/>
+<node CREATED="1428680303097" ID="ID_1181800616" MODIFIED="1428680314078" TEXT="If tagable it may be identified"/>
+<node CREATED="1428680315248" ID="ID_1689575981" MODIFIED="1428680324437" TEXT="Must be untraceable">
+<node CREATED="1428680940619" ID="ID_1612528262" MODIFIED="1428681892250" TEXT="Intermediate Peer partners should be unknown"/>
+</node>
+</node>
+<node CREATED="1428680326561" ID="ID_825227741" MODIFIED="1428682315936" TEXT="Non-replayable">
+<icon BUILTIN="full-2"/>
+<node CREATED="1428680337288" ID="ID_103357623" MODIFIED="1428680373843" TEXT="It may be identified if replayable">
+<node CREATED="1428680376406" ID="ID_506986113" MODIFIED="1428680380642" TEXT="Statistically"/>
+</node>
+<node CREATED="1428680732772" ID="ID_623597966" MODIFIED="1428680742866" TEXT="May be used to flood the system"/>
+</node>
+<node CREATED="1428680382181" ID="ID_1558006241" MODIFIED="1428681956503" TEXT="Unable to identify endpoints">
+<icon BUILTIN="full-1"/>
+<node CREATED="1428680483520" ID="ID_481374319" MODIFIED="1428680532307" TEXT="If endpoints are identifiable then their anonymity is attackable"/>
+</node>
+<node CREATED="1428680535358" ID="ID_877577605" MODIFIED="1428682181174" TEXT="Does not rely on trust of infrastructure">
+<icon BUILTIN="full-5"/>
+<node CREATED="1428680566141" ID="ID_423620927" MODIFIED="1428680601551" TEXT="If the system relys on trust of infrastructure it makes it an easy target">
+<node CREATED="1428680605203" ID="ID_1476185586" MODIFIED="1428681250836" TEXT="Owners of infrastructure may be blackmailed"/>
+<node CREATED="1428680614490" ID="ID_802329645" MODIFIED="1428681278232" TEXT="Intrested parties build infrastructure to undermine the anonymity of the system"/>
+</node>
+<node CREATED="1428680805473" ID="ID_1114441418" MODIFIED="1428680838724" TEXT="They may differentiate between message and decoys"/>
+</node>
+<node CREATED="1428680655848" ID="ID_220097510" MODIFIED="1428682201615" TEXT="Transmited messages should be simple">
+<icon BUILTIN="full-6"/>
+<node CREATED="1428680669896" ID="ID_103175288" MODIFIED="1428680878146" TEXT="If relaying on third party services it makes them suspectible to side channel attacks and buging."/>
+</node>
+<node CREATED="1428682007719" ID="ID_688855442" MODIFIED="1428682226678" TEXT="Missused">
+<icon BUILTIN="full-7"/>
+<node CREATED="1428682012225" ID="ID_845436631" MODIFIED="1428682015293" TEXT="For Spam"/>
+<node CREATED="1428682015639" ID="ID_128448967" MODIFIED="1428682021628" TEXT="For Blackmailing"/>
+</node>
+<node CREATED="1428682081108" ID="ID_1538552424" MODIFIED="1428682276758" TEXT="No central infrastructure">
+<icon BUILTIN="full-8"/>
+<node CREATED="1428682090195" ID="ID_393537488" MODIFIED="1428682098081" TEXT="Single point of failure"/>
+<node CREATED="1428682098467" ID="ID_1566971102" MODIFIED="1428682104368" TEXT="attackable"/>
+<node CREATED="1428682105594" ID="ID_248334000" MODIFIED="1428682113289" TEXT="Must be trusted"/>
+</node>
+<node CREATED="1428682290818" ID="ID_363915699" MODIFIED="1428682297994" TEXT="Reliable">
+<icon BUILTIN="full-9"/>
+</node>
+</node>
+<node CREATED="1428681040118" ID="ID_1386316765" MODIFIED="1428681773894" POSITION="left" TEXT="Requried">
+<node CREATED="1428680983800" ID="ID_1980188818" MODIFIED="1428682306230" TEXT="untraceable return adresses">
+<icon BUILTIN="full-9"/>
+<node CREATED="1428680995184" ID="ID_1556716454" MODIFIED="1428681005972" TEXT="To transmit system messages"/>
+<node CREATED="1428681006529" ID="ID_1316362156" MODIFIED="1428682167600" TEXT="To pay costs and react to requests"/>
+</node>
+<node CREATED="1428681060029" ID="ID_707504415" MODIFIED="1428681069042" TEXT="Electronic &quot;currency&quot;"/>
+<node CREATED="1428681071236" ID="ID_1707945292" MODIFIED="1428682264990" TEXT="multiple discardable undlinkable identities">
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1428681149136" ID="ID_1424312759" MODIFIED="1428682217270" TEXT="Unsuspicious looking messages">
+<icon BUILTIN="full-6"/>
+<node CREATED="1428681209822" ID="ID_1140669638" MODIFIED="1428681215339" TEXT="Steganography"/>
+</node>
+<node CREATED="1428680264163" ID="ID_1602723616" MODIFIED="1428682373699" TEXT="Cost and currency system">
+<icon BUILTIN="full-7"/>
+</node>
+<node CREATED="1428681776378" ID="ID_1515074987" MODIFIED="1428681975200" TEXT="replay protection">
+<icon BUILTIN="full-2"/>
+</node>
+<node CREATED="1428681785690" ID="ID_939459914" MODIFIED="1428681980624" TEXT="Tag/Tampering-protection">
+<icon BUILTIN="full-3"/>
+</node>
+<node CREATED="1428681910949" ID="ID_1794502229" MODIFIED="1428682343583" TEXT="Consistent end to end transport layer">
+<icon BUILTIN="full-1"/>
+<icon BUILTIN="full-8"/>
+</node>
+<node CREATED="1428682131731" ID="ID_158097292" MODIFIED="1428682349206" TEXT="Must &quot;misuse&quot; existing infrastructure">
+<icon BUILTIN="full-4"/>
+<icon BUILTIN="full-5"/>
+<icon BUILTIN="full-8"/>
+</node>
+</node>
+</node>
+</map>
