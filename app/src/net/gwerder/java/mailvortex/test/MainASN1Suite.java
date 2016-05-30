261cda3a71042658393e441b2a1d74af54367aac
mgwerder <mgwerder>
2016-05-30 07:52:37 +0000

    Added tests for Message to improve test coverage

--- app/src/net/gwerder/java/mailvortex/test/MainASN1Suite.java
+++ app/src/net/gwerder/java/mailvortex/test/MainASN1Suite.java
@@ -7,13 +7,15 @@ package net.gwerder.java.mailvortex.test;
 import junit.framework.JUnit4TestAdapter;
 import junit.framework.TestSuite;
 import net.gwerder.java.mailvortex.test.asn1.FuzzerTest;
+import net.gwerder.java.mailvortex.test.asn1.MessageTest;
 import net.gwerder.java.mailvortex.test.imap.*;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        FuzzerTest.class
+        FuzzerTest.class,
+        MessageTest.class
 })
 
 public class MainASN1Suite {
@@ -21,6 +23,7 @@ public class MainASN1Suite {
     public static junit.framework.Test suite() {
         final TestSuite s = new TestSuite();
         s.addTest(new JUnit4TestAdapter(FuzzerTest.class));
+        s.addTest(new JUnit4TestAdapter(MessageTest.class));
         return s;
     }
     // the class remains empty,
