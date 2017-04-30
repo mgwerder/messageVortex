fb9c955f710a7b7b930764f2f8bb010756bb4189
mgwerder <mgwerder>
2017-04-30 11:33:31 +0000

    Code quality work and added MIT license

--- app/src/net/gwerder/java/messagevortex/asn1/VortexMessage.java
+++ app/src/net/gwerder/java/messagevortex/asn1/VortexMessage.java
@@ -1,4 +1,26 @@
 package net.gwerder.java.messagevortex.asn1;
+/***
+ * Copyright (c) 2018 Martin Gwerder (martin@gwerder.net)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ ***/
+
 
 import net.gwerder.java.messagevortex.asn1.encryption.DumpType;
 import org.bouncycastle.asn1.*;
@@ -60,7 +82,7 @@ public class VortexMessage extends Block {
     public Prefix setPrefix(Prefix pre) {
         if( pre==null ) {
             throw new NullPointerException( "Prefix may not be null" );
-        };
+        }
         Prefix old=getPrefix();
         prefix=pre;
         return old;
@@ -110,11 +132,11 @@ public class VortexMessage extends Block {
         }
     }
 
-    public ASN1Object toASN1Object() throws IOException,NoSuchAlgorithmException,ParseException {
+    public ASN1Object toASN1Object() throws IOException {
         return toASN1Object( null, DumpType.PUBLIC_ONLY );
     }
 
-    public ASN1Object toASN1Object(AsymmetricKey identityKey, DumpType dt) throws IOException,NoSuchAlgorithmException,ParseException {
+    public ASN1Object toASN1Object(AsymmetricKey identityKey, DumpType dt) throws IOException {
         // Prepare encoding
         Logger.getLogger("VortexMessage").log(Level.FINER,"Executing toASN1Object()");
 
@@ -194,7 +216,9 @@ public class VortexMessage extends Block {
      * @return the long representation of the byte array
      */
     public static long getBytesAsInteger(byte[] b) {
-        if(b==null || b.length<1 || b.length>8) throw new IllegalArgumentException( "byte array must contain exactly four bytes" );
+        if(b==null || b.length<1 || b.length>8) {
+            throw new IllegalArgumentException( "byte array must contain exactly four bytes" );
+        }
         long ret=0;
         for(int i=0;i<b.length;i++) {
             ret|=((long)(b[i]&0xFF))<<(i*8);
