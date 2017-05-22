ea62b110441fccd5d3f683c24a4efb8f318722d6
Martin Gwerder <Martin@gwerder.net>
2017-05-22 06:15:55 +0200

    fixed ant build

--- app/test/net/gwerder/java/messagevortex/test/routing/FullMessageTest.java
+++ app/test/net/gwerder/java/messagevortex/test/routing/FullMessageTest.java
@@ -7,7 +7,6 @@ import net.gwerder.java.messagevortex.test.TestHelper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-import sun.plugin2.message.Message;
 
 import java.io.IOException;
 import java.util.Arrays;
