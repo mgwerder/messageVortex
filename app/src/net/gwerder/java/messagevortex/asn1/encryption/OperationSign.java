d3aaafc9beab2467e801e0f1d0d1101ddda262e4
Martin Gwerder <Martin@gwerder.net>
2017-12-22 14:50:31 +0100

    moved sonar sources to git tree

---
+++ app/src/net/gwerder/java/messagevortex/asn1/encryption/OperationSign.java
@@ -0,0 +1,12 @@
+package net.gwerder.java.messagevortex.asn1.encryption;
+
+/**
+ * Created by martin.gwerder on 22.12.2017.
+ */
+public class OperationSign {
+
+    public static getSetSign() {
+
+    }
+
+}
