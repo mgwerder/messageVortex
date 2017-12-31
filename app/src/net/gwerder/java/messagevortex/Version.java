e6a5bb89dac8f4b179850271a46447d9e0820069
Martin Gwerder <martin@gwerder.net>
2017-12-31 13:43:19 +0100

    updated tags

--- app/src/net/gwerder/java/messagevortex/Version.java
+++ app/src/net/gwerder/java/messagevortex/Version.java
@@ -26,7 +26,7 @@ public class Version {
     private static final int    MAJOR       = 1; //@major@
     private static final int    MINOR       = 0; //@minor@
     private static final int    REVISION    = 0; //@revision@
-    private static final String GIT_BUILD   = "\$Id\$"; //@build@
+    private static final String GIT_BUILD   = "$Id$"; //@build@
     private static final String BUILD       = SVN_BUILD.replaceAll( "\\$Id$$","" );
 
     private static final String VERSION     = MAJOR + "." + MINOR + "." + REVISION;
