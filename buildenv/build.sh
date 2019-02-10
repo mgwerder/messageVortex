ca70e16c75343894518119617d7118b470cc3aa5
Martin Gwerder <martin@gwerder.net>
2019-02-10 01:17:47 +0100

    modified build file

--- buildenv/build.sh
+++ buildenv/build.sh
@@ -2,12 +2,15 @@
 
 dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 
-# build the image if needed
-sudo docker build -t messagevortexbuild .
+(
+	cd $dir
+	# build the image if needed
+	sudo docker build -t messagevortexbuild .
 
-# stop old containers if needed
-sudo docker stop messageVortexBuild
-sudo docker rm messageVortexBuild
+	# stop old containers if needed
+	sudo docker stop messageVortexBuild
+	sudo docker rm messageVortexBuild
 
-sudo docker run -it --name devtest messageVortexBuild --mount type=bind,source="$dir/..",target=/var/tmp/messagevortex messagevortexbuild:latest
+	sudo docker run -it --name messageVortexBuild --mount type=bind,source="$dir/..",target=/var/tmp/messagevortex messagevortexbuild:latest
+)
 
