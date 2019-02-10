022c4501e073e5ea61cedfa39dfc7186a577768e
Martin Gwerder <martin@gwerder.net>
2019-02-10 17:34:49 +0100

    made sure that a docker create fails the build script

--- buildenv/build.sh
+++ buildenv/build.sh
@@ -5,14 +5,10 @@ dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 (
 	cd $dir
 	# build the image if needed
-	sudo docker build -t messagevortexbuild .
-
-	id=$(sudo docker create -t --mount type=bind,source="$dir/..",target=/var/tmp/messagevortex "$@" messagevortexbuild:latest)
-	echo "Created container with ID $id"
-	
-	
-	sudo docker start -a $id
-	
+	sudo docker build -t messagevortexbuild . && \
+	id=$(sudo docker create -t --mount type=bind,source="$dir/..",target=/var/tmp/messagevortex "$@" messagevortexbuild:latest) && \
+	echo "Created container with ID $id" && \
+	sudo docker start -a $id && \
 	sudo docker rm $id
 )
 
