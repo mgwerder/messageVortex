1e9c5e203dec12a0664aae84ec112c6d8aa82443
Martin Gwerder <martin@gwerder.net>
2019-02-10 10:32:19 +0100

    added concurrency for build

--- buildenv/build.sh
+++ buildenv/build.sh
@@ -7,10 +7,12 @@ dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 	# build the image if needed
 	sudo docker build -t messagevortexbuild .
 
-	# stop old containers if needed
-	sudo docker stop messageVortexBuild
-	sudo docker rm messageVortexBuild
-
-	sudo docker run -it --name messageVortexBuild --mount type=bind,source="$dir/..",target=/var/tmp/messagevortex "$@" messagevortexbuild:latest
+	id=$(sudo docker create --mount type=bind,source="$dir/..",target=/var/tmp/messagevortex messagevortexbuild:latest)
+	echo "Created container with ID $id"
+	
+	
+	sudo docker start -it "$@" $id
+	
+	sudo docker rm $id
 )
 
