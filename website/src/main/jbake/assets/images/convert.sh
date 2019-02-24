31db01ae9fbb6ac0c05e338aefacb8452e6f9a61
Martin Gwerder <martin@gwerder.net>
2019-02-24 16:41:51 +0100

    added huge image for book cover and made conversion reproducible

---
+++ website/src/main/jbake/assets/images/convert.sh
@@ -0,0 +1,6 @@
+inkscape -z -e MessageVortexLogo_huge.png -w 14360 -h 15000 --export-background-opacity=0 --without-gui MessageVortexLogo.svg && \
+inkscape -z -e MessageVortexLogo_big.png -w 1436 -h 1500 --export-background-opacity=0 --without-gui MessageVortexLogo.svg && \
+inkscape -z -e MessageVortexLogo.png -w 128 -h 128 --export-background-opacity=0 --without-gui MessageVortexLogo.svg 
+
+
+ 
