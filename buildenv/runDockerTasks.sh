6241ea6fb2f6b364ce77c9a33f06783dcdc5af74
Martin Gwerder <martin@gwerder.net>
2019-02-25 18:49:13 +0100

    added JAVA_HOME to container

--- buildenv/runDockerTasks.sh
+++ buildenv/runDockerTasks.sh
@@ -1,9 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 (
         cd $dir/..
         echo "dir is $dir (script is $0)"
-        MAVEN_CONFIG=/var/lib/maven/ MAVEN_OPTS="-Xmx2048m -XX:MaxPermSize=512m -XX:MaxDirectMemorySize=512m -XX:+HeapDumpOnOutOfMemoryError -Dfile.encoding=UTF-8" mvn package ${MAVEN_ARGS}
+        export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/ 
+        export MAVEN_CONFIG=/var/lib/maven/ 
+        export MAVEN_OPTS="-Xmx2048m -XX:MaxPermSize=512m -XX:MaxDirectMemorySize=512m -XX:+HeapDumpOnOutOfMemoryError -Dfile.encoding=UTF-8" 
+        mvn package ${MAVEN_ARGS}
         $dir/mkindex.sh
 )
