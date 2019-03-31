0294d8fef1c3e71dc862d6fa4a4163bad1989756
Martin Gwerder <martin@gwerder.net>
2019-03-31 17:23:24 +0200

    Fixed build error

--- buildenv/build.sh
+++ buildenv/build.sh
@@ -42,7 +42,7 @@ dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 		ls -la $tmpdir/*
 	) && \
 	trap "rm -rf $tmpdir" EXIT && \
-	(mkdir $dir/../target/mavencache;/bin/true) && \
+	(mkdir /var/tmp/dockermavencache;/bin/true) && \
 	( cd $tmpdir; tar -xvf $dir/mavenfiles.tar ) && \
 	id=$(sudo docker create -t \
 	     --mount type=bind,source="$tmpdir/",target=/var/tmp/messagevortex/ \
@@ -52,7 +52,7 @@ dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 	     --mount type=bind,readonly,source="$dir/../rfc/src/",target=/var/tmp/messagevortex/rfc/src/ \
 	     --mount type=bind,readonly,source="$dir/../buildenv/",target=/var/tmp/messagevortex/buildenv/ \
 	     --mount type=bind,readonly,source="$dir/../application-core-library/src/",target=/var/tmp/messagevortex/application-core-library/src/ \
-	     --mount type=bind,source="$dir/../target/mavencache",target=/var/lib/maven/ \
+	     --mount type=bind,source="/var/tmp/dockermavencache",target=/var/lib/maven/ \
 	     "$@" messagevortexbuild:latest) && \
 	echo "Created container with ID $id" && \
 	sudo docker start -a $id && \
