fc801a0ef12afa5eaa5b5dae5f06c5aa09931eaa
Martin Gwerder <martin@gwerder.net>
2019-05-13 09:54:04 +0200

    Added low anon recipes/LowAnonRecipe.java (not yet functional)

---
+++ application-core-library/src/main/java/net/messagevortex/blender/recipes/LowAnonRecipe.java
@@ -0,0 +1,55 @@
+package net.messagevortex.blender.recipes;
+
+import java.util.List;
+import java.util.Vector;
+import net.messagevortex.ExtendedSecureRandom;
+import net.messagevortex.asn1.IdentityStoreBlock;
+import net.messagevortex.asn1.RoutingBlock;
+
+public class LowAnonRecipe extends BlenderRecipe {
+
+  public LowAnonRecipe(String section) {
+    // no parameters required
+  }
+
+  @Override
+  public boolean isAppliable(List<IdentityStoreBlock> anonSet) {
+    if (anonSet != null && anonSet.size() > 0 && anonSet.size() > 4) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  @Override
+  public RoutingBlock applyRecipe(List<IdentityStoreBlock> anonSet, IdentityStoreBlock from,
+                                  IdentityStoreBlock to) {
+    // select random order
+    List<IdentityStoreBlock> set = new Vector<>();
+    //set.add(from);
+    set.add(to);
+    set.addAll(anonSet);
+
+    // select random order
+    int[] order = new int[set.size()];
+    for (int i = 0; i < order.length; i++) {
+      int ni = ExtendedSecureRandom.nextInt(order.length - i);
+      for (int j = i-1; j > 0; j--) {
+        if (order[j] <= ni ) {
+          ni ++;
+        }
+      }
+      order[i] = ni;
+    }
+
+
+    // split message at start
+
+    // send packages along the line
+
+    // add dead ends
+
+    return null;
+  }
+
+}
