e2fe2e6c1361a99c734ef03e4ad8aa2f6212d236
Martin Gwerder <Martin@gwerder.net>
2019-09-23 16:47:37 +0200

    Eradicated google auto services (modified maven process to compile processors first)

--- application-core-library/src/main/java/net/messagevortex/asn1/annotator/AsnOneBuilder.java
+++ application-core-library/src/main/java/net/messagevortex/asn1/annotator/AsnOneBuilder.java
@@ -4,22 +4,17 @@ import java.util.LinkedHashSet;
 import java.util.Set;
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.ProcessingEnvironment;
-import javax.annotation.processing.Processor;
 import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.TypeElement;
 
-import com.google.auto.service.AutoService;
-import opennlp.tools.sentdetect.lang.Factory;
-
 @SupportedSourceVersion(SourceVersion.RELEASE_7)
 @SupportedAnnotationTypes({
         // Set of full qualified annotation type names
-        
 })
-@AutoService(Processor.class)
+
 public class AsnOneBuilder extends AbstractProcessor {
   
   @Override
@@ -30,7 +25,7 @@ public class AsnOneBuilder extends AbstractProcessor {
   @Override
   public Set<String> getSupportedAnnotationTypes() {
     Set<String> annotataions = new LinkedHashSet<>();
-    annotataions.add(Factory.class.getCanonicalName());
+    annotataions.add(this.getClass().getCanonicalName());
     return annotataions;
   }
   
