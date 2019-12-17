70dfd41d582ccc0b23578222b637dcad630da018
Martin Gwerder <martin@gwerder.net>
2019-12-17 08:09:47 +0100

    fixed yaml test

--- application-core-library/src/main/java/net/messagevortex/asn1/yaml/IdentityStoreYamlHandler.java
+++ application-core-library/src/main/java/net/messagevortex/asn1/yaml/IdentityStoreYamlHandler.java
@@ -3,22 +3,21 @@ package net.messagevortex.asn1.yaml;
 import java.util.List;
 import java.util.Vector;
 import net.messagevortex.asn1.IdentityStore;
-import org.snakeyaml.engine.v2.api.RepresentToNode;
-import org.snakeyaml.engine.v2.common.FlowStyle;
-import org.snakeyaml.engine.v2.nodes.MappingNode;
-import org.snakeyaml.engine.v2.nodes.Node;
-import org.snakeyaml.engine.v2.nodes.NodeTuple;
-import org.snakeyaml.engine.v2.nodes.Tag;
 import org.yaml.snakeyaml.constructor.Constructor;
+import org.yaml.snakeyaml.nodes.MappingNode;
+import org.yaml.snakeyaml.nodes.Node;
+import org.yaml.snakeyaml.nodes.NodeTuple;
+import org.yaml.snakeyaml.nodes.Tag;
+import org.yaml.snakeyaml.representer.Represent;
 
-public class IdentityStoreYamlHandler extends Constructor implements RepresentToNode {
+public class IdentityStoreYamlHandler extends Constructor implements Represent {
   
   @Override
   public Node representData(Object data) {
     IdentityStore identityStore = (IdentityStore) data;
     List<NodeTuple> value = new Vector<>();
     
-    return new MappingNode(new Tag("!identityStore"),value,FlowStyle.AUTO);
+    return new MappingNode(new Tag("!identityStore"),value,true);
   }
   
   
