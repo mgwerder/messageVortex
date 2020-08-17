0439110e03e810d380aa661f2a9f820a15361424
Martin Gwerder <martin@gwerder.net>
2020-08-17 17:28:53 +0200

    sparse save

---
+++ application-core-library/src/main/java/net/messagevortex/commandline/CommandLineHandlerExamples.java
@@ -0,0 +1,26 @@
+package net.messagevortex.commandline;
+
+import java.util.concurrent.Callable;
+import java.util.logging.Level;
+import net.messagevortex.MessageVortexConfig;
+import net.messagevortex.MessageVortexLogger;
+import net.messagevortex.NotImplementedException;
+import picocli.CommandLine;
+
+
+@CommandLine.Command(
+    description = "Create example snippets",
+    name = "example",
+    aliases = {"ex"},
+    mixinStandardHelpOptions = true,
+    subcommands = {
+        CommandLineHandlerExamplesAddress.class
+    }
+)
+public class CommandLineHandlerExamples implements Callable<Integer> {
+  public static final String DEFAULT_FILENAME = "identityStore.cfg";
+
+  public Integer call() throws Exception {
+    throw new NotImplementedException();
+  }
+}
