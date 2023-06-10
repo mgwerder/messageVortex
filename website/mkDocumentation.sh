f3777775801da77ccd29412b9e19bc6b28cc1dfa
Martin Gwerder <martin@gwerder.net>
2023-06-10 10:06:59 +0200

    removed issues with homepage building (updated maven plugins)

--- website/mkDocumentation.sh
+++ website/mkDocumentation.sh
@@ -28,7 +28,7 @@ mkdir -p ${WWWDIR}/devel/ 2>/dev/null
 cp -u $dir/../application-core-library/target/MessageVortex-core.jar ${WWWDIR}/devel/messageVortex_development_core.jar
 cp -u $dir/../rfc/src/xml2rfc/draft-gwerder-*.xml  ${WWWDIR}/devel/
 #cp $dir/target/messageVortex_apidoc.zip ${WWWDIR}/devel/
-(cd ${WWWDIR}/devel/; mkdir apidoc 2>/dev/null; cd apidoc; unzip -qox ..//messageVortex_apidoc.zip)
+(cd ${WWWDIR}/devel/; mkdir apidoc 2>/dev/null; cd apidoc; unzip -qox ../messageVortex_apidoc.zip)
 cp -u $dir/../rfc/target/xml2rfc/draft-gwerder-*.{xmlflat,pdf,ps,epub,mobi,txt,legacytxt,rawtxt,html} ${WWWDIR}/devel/
 cp -u $dir/../rfc/src/xml2rfc/rfc2629.xslt ${WWWDIR}/devel/
 #cp -u $dir/../thesis/target/main/latex/messageVortex.pdf ${WWWDIR}/devel/
